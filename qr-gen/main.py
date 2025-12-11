60c1c494c5b31fd784635cdf739ff4c323e9b2da
Mose Schmiedel <mose@schmiednet.de>
2025-12-11 01:11:41 +0100

    App link QR code generation

--- qr-gen/main.py
+++ qr-gen/main.py
@@ -1,13 +1,26 @@
 import sys
+
+import qrcode
 from song import SongDB
 
 if __name__ == "__main__":
     songs = SongDB()
     songs.from_csv("songs.csv")
-    for s in songs:
-        print(s)
 
     if sys.argv[1] == "download":
         songs.download_yt()
     elif sys.argv[1] == "qr":
         songs.generate_qr_codes("codes")
+    elif sys.argv[1] == "site-qr":
+        qr = qrcode.QRCode(
+            version=None,
+            error_correction=qrcode.constants.ERROR_CORRECT_Q,
+            box_size=10,
+            border=4,
+        )
+        qr_data = "https://lobster.jugule.de"
+        qr.add_data(qr_data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("lobster_site_qr.png")
