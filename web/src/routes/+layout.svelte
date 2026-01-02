91f151c492f5a66729ab749c0ec0c14ab7844015
Timon Förster <timon.foerster@ahfcampus.de>
2026-01-02 14:48:19 +0100

    New favicon

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="shortcut icon" href={favicon} type="image/x-icon">
+	<link rel="icon" href={favicon} type="image/x-icon">
 </svelte:head>
 
 {@render children()}
